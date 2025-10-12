a39e988886860c38081658f86c43efa680749fc8
ChaitaleePatil <163013839+ChaitaleePatil@users.noreply.github.com>
2025-10-12 10:23:16 +0530

    comment out line 143 in database/crud.py

--- sections/Earnings_Section.py
+++ sections/Earnings_Section.py
@@ -140,7 +140,7 @@ def show_page():
 
     companies = [k for k in config.keys() if k != "GLOBAL_DEFAULTS"]
     selected_company = st.selectbox("Select Company", companies)
-    api_key = 'AIzaSyB_t9RrIGjmp_Fd8_PrnGnZAfDQ2rqyDOs'  # Hardcoded API key
+    #api_key = 'AIzaSyB_t9RrIGjmp_Fd8_PrnGnZAfDQ2rqyDOs'  
 
     selected_config = config[selected_company]
 
